5cead7fc9df5e661996a8e03963b0542ad23223d
Simon Gomizelj <simongmzlj@gmail.com>
2010-03-16 22:42:58 -0400

    this simply echos requests now as a test, will start copying in http header parsing code

--- smallhttp.c
+++ smallhttp.c
@@ -2,12 +2,20 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include "server.h"
 
 int readsocket(int id, int fd)
 {
-	fprintf(stderr, "got a connection\n");
-	return 1;
+	char buf[2048];
+	int r = read(fd, buf, 2048);
+	buf[r] = '\0';
+
+	printf("%d: %s", id, buf);
+
+	if (r == 0)
+		return 1;
+	return 0;
 }
 
 int smallhttp_main(int argc, char *argv[], httpd *ops)
@@ -15,7 +23,6 @@ int smallhttp_main(int argc, char *argv[], httpd *ops)
 	int port = argc > 1 ? atoi(argv[1]) : 8080;
 
 	sockserver *srv = startserver(port);
-	while (1) {
+	while (1)
 		pollserver(srv, 0, readsocket);
-	}
 }
