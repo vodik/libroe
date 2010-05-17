0ed92fa29ffefcec0c07f524740fb8ef96ec7e77
Simon Gomizelj <simongmzlj@gmail.com>
2010-05-16 22:02:50 -0400

    corrected typo: hashashtable -> hashtable

--- util/hashtable.h
+++ util/hashtable.h
@@ -1,24 +1,24 @@
 #ifndef SMALLHTTP_UTIL_HASHTABLE
 #define SMALLHTTP_UTIL_HASHTABLE
 
-typedef struct hashashtable hashashtable;
+typedef struct hashtable hashashtable;
 typedef unsigned int (*hashfunc)(const char *);
 
 struct keypair;
 
-struct hashashtable {
+struct hashtable {
 	unsigned int size;
 	struct keypair **nodes;
 	hashfunc hasher;
 };
 
-void hashtable_init(unsigned int size, hashfunc hasher, hashashtable *tbl);
-void hashtable_free(hashashtable *, int freedata);
+void hashtable_init(unsigned int size, hashfunc hasher, hashtable *tbl);
+void hashtable_free(hashtable *, int freedata);
 
-void hashtable_insert(hashashtable *, const char *key, void *data);
-int hashtable_remove(hashashtable *, const char *key);
-void *hashtable_get(const hashashtable *, const char *key);
+void hashtable_insert(hashtable *, const char *key, void *data);
+int hashtable_remove(hashtable *, const char *key);
+void *hashtable_get(const hashtable *, const char *key);
 
-void hashtable_resize(hashashtable *, unsigned int size);
+void hashtable_resize(hashtable *, unsigned int size);
 
 #endif
