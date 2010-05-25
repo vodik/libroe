e02d6ecbfe5117d51306da25c717314ab72da415
Simon Gomizelj <simongmzlj@gmail.com>
2010-05-25 05:37:42 -0400

    solid base for the new parser. still needs to be intergrated. the new parser does not allocate any memory and should also be generally faster

--- request/parser.c
+++ request/parser.c
@@ -1,36 +1,169 @@
-#include "http_parser.h"
+#include "parser.h"
 
-state_fn state_method, state_path, state_version, state_header, state_value;
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
 
-int state_method(http_parser *parser)
+state_fn state_method, state_path, state_version, state_header, state_field;
+
+int state_method(struct state_t *state, char *buf, size_t nbytes, event_data_t *evt)
 {
-	char *b = parser->buf;
-	size_t nbytes = parser->nbytes;
+	int read = 0;
 
-	while (nbytes-- > 0 && *b != ' ')
-		 ++b;
+	while (state->len-- > 0 && (*buf = *state->buf++) != ' ' && read < nbytes) {
+		++read;
+		++buf;
+	}
 
-	
+	evt->type = HTTP_DATA_METHOD;
+	state->next = state_path;
+
+	return read;
+}
+
+int state_path(struct state_t *state, char *buf, size_t nbytes, event_data_t *evt)
+{
+	int read = 0;
+
+	while (state->len-- > 0 && (*buf = *state->buf++) != ' ' && read < nbytes) {
+		++read;
+		++buf;
+	}
+
+	evt->type = HTTP_DATA_PATH;
+	state->next = state_version;
+
+	return read;
+}
+
+int state_version(struct state_t *state, char *buf, size_t nbytes, event_data_t *evt)
+{
+	int read = 0;
+
+	while (state->len-- > 0 && (*buf = *state->buf++) != '\r' && read < nbytes) {
+		++read;
+		++buf;
+	}
+	++state->buf;
+	--state->len;
+
+	evt->type = HTTP_DATA_VERSION;
+	state->next = state_header;
+
+	return read;
+}
+
+int state_header(struct state_t *state, char *buf, size_t nbytes, event_data_t *evt)
+{
+	int read = 0;
+
+	assert(state->len < 2000);
+	if (state->len == 2) {
+		state->len = 0;
+		state->next = NULL;
+		return 0;
+	}
+
+	while (state->len-- > 0 && (*buf = *state->buf++) != ':' && read < nbytes) {
+		++read;
+		++buf;
+	}
+	++state->buf;
+	--state->len;
+
+	evt->type = HTTP_DATA_HEADER;
+	state->next = state_field;
+
+	return read;
+}
+
+int state_field(struct state_t *state, char *buf, size_t nbytes, event_data_t *evt)
+{
+	int read = 0;
+
+	assert(state->len < 2000);
+
+	while (state->len-- > 0 && (*buf = *state->buf++) != '\r' && read < nbytes) {
+		++read;
+		++buf;
+	}
+	++state->buf;
+	--state->len;
+
+	evt->type = HTTP_DATA_FIELD;
+	state->next = state_header;
+
+	return read;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-void http_parser_init(http_parser *)
+void http_parser_init(http_parser *parser)
 {
+	//parser->buffer = malloc(bufsize);
+	parser->state.next = state_method;
 }
 
-void http_parser_set_buffer(http_parser *parser, const char *buf, size_t nbytes)
+void http_parser_set_buffer(http_parser *parser, char *buf, size_t nbytes)
 {
-	parser->buf = buf;
-	parser->nbytes = nbytes;
+	parser->state.buf = buf;
+	parser->state.len = nbytes;
 }
 
-struct http_event *http_parser_next_event(http_parser *)
+int http_parser_next_event(http_parser *parser, char *buf, size_t nbytes, event_data_t *evt)
 {
-	if (parser->nbytes < 0 && parser->next_state == NULL)
-		return NULL;
+	//http_parser_event *event = &parser->event;
+	int event = -1;
 
-	static http_event event;
-	parser->next_state(parser, &event);
-	return &event;
+	if (parser->len > 0 && parser->state.next)
+		event = parser->state.next(&parser->state, buf, nbytes, evt);
+
+	return event;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+static const char test[] =
+	"GET / HTTP/1.1\r\n"
+	"Fuck: off\r\n"
+	"\r\n";
+
+int main(int argc, char *argv[])
+{
+	char buf[1024];
+	char *temp = strdup(test);
+
+	http_parser parser;
+	event_data_t data;
+	int read;
+
+	http_parser_init(&parser);
+	http_parser_set_buffer(&parser, temp, strlen(temp));
+	printf("strlen: %d\n", strlen(temp));
+	
+	while ((read = http_parser_next_event(&parser, buf, 1024, &data)) > 0) {
+		buf[read] = '\0';
+		switch (data.type) {
+			case HTTP_DATA_METHOD:
+				printf("we got a method!\n");
+				break;
+			case HTTP_DATA_PATH:
+				printf("we got a path!\n");
+				break;
+			case HTTP_DATA_VERSION:
+				printf("we got version information!\n");
+				break;
+			case HTTP_DATA_HEADER:
+				printf("we got a header\n");
+				break;
+			case HTTP_DATA_FIELD:
+				printf("we got a field\n");
+				break;
+			default:
+				printf("???\n");
+		}
+		printf("read: %d\nbuf: %s\n\n", read, buf);
+	}
+	return 0;
 }
--- request/parser.h
+++ request/parser.h
@@ -1,10 +1,11 @@
-struct http_parser;
 #ifndef SMALLHTTP_REQUEST_PARSER
 #define SMALLHTTP_REQUEST_PARSER
 
+#include <stddef.h>
+
 struct event_t;
 struct state_t;
-typedef int state_fn(struct state_t *, const char **, int *);
+//typedef int state_fn(struct state_t *);/*, const char **, int *);*/
 
 enum http_methods {
 	HTTP_DELETE = 0x00,
@@ -15,10 +16,12 @@ enum http_methods {
 };
 
 enum http_parser_data {
+	HTTP_DATA_METHOD,
 	HTTP_DATA_PATH,
 	HTTP_DATA_QUERY,
 	HTTP_DATA_URL,
 	HTTP_DATA_FRAGMENT,
+	HTTP_DATA_VERSION,
 	HTTP_DATA_HEADER,
 	HTTP_DATA_FIELD,
 };
@@ -28,25 +31,40 @@ enum http_parser_events {
 	HTTP_EVENT_UPGRADE,
 };
 
-struct http_event {
+typedef struct {
 	enum http_parser_data type;
 	const char *buf;
 	size_t nbytes;
 	int done;
-};
+} http_parser_event;
 
-struct http_parser_state {
+/*struct http_parser_state {
 	state_fn *next_state;
 	struct http_event *event;
-}
+};*/
 
 typedef struct {
-	struct http_parser_state state;
-	struct http_event event;
+	int type;
+} event_data_t;
+
+struct http_parser;
+typedef int state_fn(struct state_t *state, char *buf, size_t nbytes, event_data_t *data);
+
+struct state_t {
+	char *buf;
+	size_t len;
+	state_fn *next;
+};
+
+typedef struct http_parser {
+	struct state_t state;
+	http_parser_event event;
+	char *buf;
+	size_t len;
 } http_parser;
 
-void http_parser_init(http_parser *);
-void http_parser_set_buffer(http_parser *, const char *buf, size_t nbytes);
-struct http_event *http_parser_next_event(http_parser *);
+/*void http_parser_init(http_parser *);
+void http_parser_set_buffer(http_parser *, char *buf, size_t nbytes);
+struct http_event *http_parser_next_event(http_parser *);*/
 
 #endif
