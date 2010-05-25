7e8a1769c41a480fabce3cb4e1689d3ffce92c66
Simon Gomizelj <simongmzlj@gmail.com>
2010-05-25 06:50:38 -0400

    basic integrations. doesn't work, trace is garbage

--- request/parser.h
+++ request/parser.h
@@ -3,10 +3,6 @@
 
 #include <stddef.h>
 
-struct event_t;
-struct state_t;
-//typedef int state_fn(struct state_t *);/*, const char **, int *);*/
-
 enum request_methods {
 	HTTP_DELETE = 0x00,
 	HTTP_GET    = 0x01,
@@ -26,28 +22,11 @@ enum request_parser_data {
 	HTTP_DATA_FIELD,
 };
 
-enum request_parser_events {
-	HTTP_EVENT_DONE,
-	HTTP_EVENT_UPGRADE,
-};
-
-typedef struct {
-	enum request_parser_data type;
-	const char *buf;
-	size_t nbytes;
-	int done;
-} request_parser_event;
-
-/*struct request_parser_state {
-	state_fn *next_state;
-	struct request_event *event;
-};*/
-
 typedef struct {
 	int type;
 } event_data_t;
 
-struct request_parser;
+struct state_t;
 typedef int state_fn(struct state_t *state, char *buf, size_t nbytes, event_data_t *data);
 
 struct state_t {
