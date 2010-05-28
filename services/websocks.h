14eec66d16c5efa248f98ee774becae7f7209fb4
Simon Gomizelj <simongmzlj@gmail.com>
2010-05-28 02:45:37 -0400

    added a string buffer for more robust handling of strings

--- services/websocks.h
+++ services/websocks.h
@@ -17,6 +17,6 @@ struct ws_ops {
 void websocks_start(struct service_t *ws, poll_mgmt_t *mgmt, struct ws_ops *ops);
 
 void ws_close(ws_t *ws);
-void ws_send(ws_t *ws, const char *buf, size_t nbytes);
+size_t ws_send(ws_t *ws, const char *buf, size_t nbytes);
 
 #endif
