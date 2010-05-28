8525b4c0984515ec1759042bf939ff8d9988c49d
Simon Gomizelj <simongmzlj@gmail.com>
2010-05-28 01:40:50 -0400

    ws_t data structure added

--- services/websocks.h
+++ services/websocks.h
@@ -3,14 +3,20 @@
 
 #include "services.h"
 
+typedef struct {
+	int fd;
+	char *path;
+	int status;
+} ws_t;
+
 struct ws_ops {
 	int port;
-	//void (*onrequest)(http_conn *conn);
+	void (*onrequest)(ws_t *ws);
 };
 
 void websocks_start(struct service_t *ws, poll_mgmt_t *mgmt, struct ws_ops *ops);
 
 void ws_close(ws_t *ws);
-size_t ws_send(ws_t *ws, const char *buf, size_t nbytes);
+void ws_send(ws_t *ws, const char *buf, size_t nbytes);
 
 #endif
