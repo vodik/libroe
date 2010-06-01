c51cde73fe9ec658eb8cd7651abdfee236d38556
Simon Gomizelj <simongmzlj@gmail.com>
2010-06-01 19:25:56 -0400

    conn_t wasn't being built

--- src/conn.c
+++ src/conn.c
@@ -1,14 +1,21 @@
 #include <conn.h>
 
+#include <stdlib.h>
+#include <assert.h>
+#include <unistd.h>
+
 conn_t *
 conn_new(size_t size, int fd)
 {
 	assert(size >= sizeof(conn_t));
 
 	conn_t *c = malloc(size);
-	c->fd = fd;
-	c->status = CONN_OPEN;
-	c->ref = 1;
+	if (c) {
+		c->fd = fd;
+		c->status = CONN_OPEN;
+		c->ref = 1;
+	}
+	return c;
 }
 
 conn_t *
@@ -22,14 +29,14 @@ void
 conn_unref(conn_t *c)
 {
 	if (--c->ref <= 0) {
-		if (c->status = CONN_OPEN)
+		if (c->status == CONN_OPEN)
 			close(c->fd);
 		free(c);
 	}
 }
 
 void
-conn_close(conn_t *c);
+conn_close(conn_t *c)
 {
 	close(c->fd);
 	c->status = CONN_CLOSED;
